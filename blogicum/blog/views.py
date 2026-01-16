96ae553917f2c6a295bbfbbc8f8d6e7af257ac39
Dmitry Meshcheryakov <Dima23232424@yandex.ru>
2026-01-16 05:41:06 +0300

    added app_name to blog.urls and pages.urls, added code to views.py to both pages and blog, 100% wrong

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    template_name = 'index.html'
+    return render(request, template_name)
+
+
+def post_detail(request, id):
+    template_name = 'detail.html'
+    return render(request, id, template_name)
+
+
+def category_posts(request, category_slug):
+    template_name = 'category.html'
+    return render(request, category_slug)
